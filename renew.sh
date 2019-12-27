8cdbd4b9b27f05c2d58a22846f0175264db1bad5
Nikolai Furmanczak <nikolai.furmanczak@gmail.com>
2019-12-27 14:51:36 +0100

    add var for lets encrypt path and remove dry run switch

--- renew.sh
+++ renew.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Default location for lets encrypt TLS certs, please change if you use 
+# a different dir
+tlscerts='/etc/letsencrypt/live/*/cert.pem'
+
 # Check if necessary tools are installed
 which date &> /dev/null
 if [ $? -eq 1 ]; then
@@ -13,15 +17,17 @@ if [ $? -eq 1 ]; then
 	exit 1
 fi
 
+# current date in iso 8601 format 
 curdate=`date --iso-8601`
 
-for file in /etc/letsencrypt/live/*/cert.pem; do
+
+for file in $tlscerts; do
 	fqdn=`openssl x509 -noout -subject -in $file | cut -d' ' -f 3`
 	enddate=`date --date="$(openssl x509 -in $file -noout -enddate | cut -d= -f 2)" --iso-8601`
 	diff=$(($(date -d "$enddate" '+%s') - $(date -d "$curdate" '+%s')))
 	diffDays=$(($diff/(3600*24)))
 
-	if [ $diffDays -lt 80 ]; then
-		/opt/letsencrypt/certbot-auto	renew --cert-name $fqdn --dry-run
+	if [ $diffDays -lt 10 ]; then
+		/opt/letsencrypt/certbot-auto	renew --cert-name $fqdn &> /dev/null 
 	fi
 done
